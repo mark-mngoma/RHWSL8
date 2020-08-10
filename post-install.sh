5595542a283ad04080c426c29ac900fb111ba867
rob3rt_ <rob3rt.233@gmail.com>
2020-08-10 17:45:50 +0800

    Fixed some problem with aliasing

--- post-install.sh
+++ post-install.sh
@@ -7,21 +7,21 @@ echo
 
 default_name="RHWSL8"
 read -p "Distro name [$default_name]: " -r distro_name
-[ -z $distro_name ] && distro_name=$default_name
+[[ -z $distro_name ]] && distro_name=$default_name
 
 echo "[*] Downloading wsldl"
 curl -L https://github.com/yuk7/wsldl/releases/download/20040300/Launcher.exe -o $distro_name.exe || exit $?
 chmod +x $distro_name.exe || exit $?
 
 ./$distro_name.exe || exit $?
-alias wsl-exec="wsl.exe -d $distro_name"
-alias wsl-term="wsl.exe -t $distro_name"
+wsl-exec() { wsl.exe -d $distro_name "$@"; }
+wsl-term() { wsl.exe -t $distro_name "$@"; }
 
-function env-setup() {
+env-setup() {
     echo "[*] Installing Server environment group"
     wsl-exec genie -i || return $?
 
-    if [ -z $rh_login ] && [ -z $rh_passwd ]; then
+    if [[ -n $rh_login ]] && [[ -n $rh_passwd ]]; then
         wsl-exec subscription-manager register --username=$rh_login --password=$rh_passwd --auto-attach || return $?
     else
         wsl-exec subscription-manager register --auto-attach || return $?
@@ -32,7 +32,7 @@ function env-setup() {
 
     echo "[*] Creating new regular user with UID 1000"
     read -p "New UNIX username: " -r wsl_username
-    wsl-exec groupdel -f docker || (code=$?; [ $code != '6' ] && return $code)
+    wsl-exec groupdel -f docker || ( code=$?; [[ $code != '6' ]] && return $code )
     wsl-exec useradd -u 1000 -G wheel $wsl_username || return $?
     wsl-exec passwd $wsl_username || return $?
 
@@ -40,7 +40,7 @@ function env-setup() {
     wsl-exec usermod -aG docker $wsl_username || return $?
 }
 
-env-setup || (code=$?; wsl-term || exit $?; exit $code)
+env-setup || ( code=$?; wsl-term || exit $?; exit $code )
 wsl-term || exit $?
 
 ./$distro_name.exe config --default-uid 1000 || exit $?
